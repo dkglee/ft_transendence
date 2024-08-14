cfae36e8ac7330401d8fdacd1f346c3213dcef89
dkglee <ideade9@gmail.com>
2024-08-14 14:41:24 +0900

    feat: game start when players all connected

--- mysite/transcendence/consumer.py
+++ mysite/transcendence/consumer.py
@@ -1,14 +1,34 @@
 import json
+import asyncio
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
+from .models import GameSession, Player
+from .service import GameServiceSingleton
+
+@sync_to_async
+def SetSessionActive(session_id, handshake):
+    session = GameSession.objects.filter(session_id=session_id).first()
+    if session:
+        # 예제에서는 handshake로 처리하지만 실제로는 다른 로직이 있을 수 있습니다.
+        session.is_active = True
+        session.save()
+    else:
+        print(f"Session {session_id} does not exist")
+        return False
+    
+    if session.players.count() == 4:
+        return True
+    else:
+        return False
 
 class GameConsumer(AsyncWebsocketConsumer):
+    session_timers = {}
+
     async def connect(self):
-        # URL에서 세션 id 추출
         self.session_id = self.scope["url_route"]["kwargs"]["session_id"]
         self.roomGroupName = f"game_{self.session_id}"
 
-        # settings에서 session_queues 가져오기
         session_queues = getattr(settings, 'SESSION_QUEUES', None)
         if session_queues:
             self.queue = session_queues.get(self.session_id)
@@ -33,20 +53,59 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        print(f"Received message: {message} for session: {self.session_id}")
         
-        # 큐를 통해 세션 스레드에 데이터 전달
-        if self.queue:
-            self.queue.put(message)
+        # 첫 메시지로 handshake 받기
+        if "handshake" in text_data_json:
+            handshake = text_data_json["handshake"]
 
+            print(f"Received handshake: {handshake} for session {self.session_id}")
+
+            bIsFull = await SetSessionActive(self.session_id, handshake)
+            
+            if bIsFull:
+                print(f"Session {self.session_id} is full. Starting game...")
+                await self.start_game(self.session_id)
+                if self.session_id in GameConsumer.session_timers:
+                    GameConsumer.session_timers[self.session_id].cancel()
+                    del GameConsumer.session_timers[self.session_id]
+            else:
+                if self.session_id not in GameConsumer.session_timers:
+                    timer_task = asyncio.create_task(self.start_game_after_timeout())
+                    GameConsumer.session_timers[self.session_id] = timer_task
+
+        # 그 외의 메시지 처리 (예: username 기반 메시지)
+        else:
+            message = text_data_json.get("message", "")
+            print(f"Received message: {message} for session: {self.session_id}")
+            
+            if self.queue:
+                self.queue.put(message)
+
+            await self.channel_layer.group_send(
+                self.roomGroupName, {
+                    "type": "sendMessage",
+                    "message": message,
+                })
+            
+    async def sendMessage(self, event):
+        message = "Pong"
+        await self.send(text_data=json.dumps({"message": message}))
+
+    async def start_game(self, session_id):
+        GameServiceSingleton().start_game(session_id)
         await self.channel_layer.group_send(
             self.roomGroupName, {
                 "type": "sendMessage",
-                "message": message,
+                "message": "Game Started",
             })
 
-    async def sendMessage(self, event):
-        # message = event["message"]
-        message = "Pong"
-        await self.send(text_data=json.dumps({"message": message}))
+    async def start_game_after_timeout(self):
+        try:
+            await asyncio.sleep(20)
+            print(f"Session {self.session_id} did not fill up in time. Starting game...")
+            await self.start_game(self.session_id)
+        except asyncio.CancelledError:
+            print(f"Session {self.session_id} was filled in time. Timer cancelled.")
+        finally:
+            if self.session_id in GameConsumer.session_timers:
+                del GameConsumer.session_timers[self.session_id]
--- mysite/transcendence/service.py
+++ mysite/transcendence/service.py
@@ -13,41 +13,49 @@ class GameServiceSingleton:
 
     def __init__(self):
         if not self._initialized:
+            self.bGameStarted = {}
             self.session_queues = self.start_service()
             self._initialized = True
 
     def start_service(self):
         # Django 앱이 완전히 로드된 후에 모델을 가져오기 위해 apps.get_model 사용
         print("Starting game service...")
-        GameSession = apps.get_model('transcendence', 'GameSession')
 
-        num_sessions = 8
-        websocket_url = "127.0.0.1"
-        websocket_port = 8001
+        try:
+            GameSession = apps.get_model('transcendence', 'GameSession')
+            num_sessions = 8
+            websocket_url = "127.0.0.1"
+            websocket_port = 8001
 
-        # 기존 세션 삭제
-        GameSession.objects.all().delete()
+            # 기존 세션 삭제
+            GameSession.objects.all().delete()
 
-        executor = ThreadPoolExecutor(max_workers=num_sessions)
-        session_queues = {}
+            executor = ThreadPoolExecutor(max_workers=num_sessions)
+            session_queues = {}
 
-        for i in range(num_sessions):
-            session_id = f"session_{i+1}"
-            GameSession.objects.create(
-                session_id=session_id,
-                websocket_url=websocket_url,
-                websocket_port=websocket_port,
-                is_active=False
-            )
-            queue = Queue()
-            session_queues[session_id] = queue
-            executor.submit(self.session_thread, session_id, queue, websocket_url, websocket_port)
+            for i in range(num_sessions):
+                session_id = f"session_{i+1}"
+                GameSession.objects.create(
+                    session_id=session_id,
+                    websocket_url=websocket_url,
+                    websocket_port=websocket_port,
+                    is_active=False
+                )
+                queue = Queue()
+                session_queues[session_id] = queue
+                self.bGameStarted[session_id] = False
+                executor.submit(self.session_thread, session_id, queue, websocket_url, websocket_port)
 
-        return session_queues
+            return session_queues
+        
+        except Exception as e:
+            print(f"Error starting game service: {e}")
 
     def session_thread(self, session_id, queue, websocket_url, websocket_port):
         # Session Logic Here
         while True:
+            if self.bGameStarted[session_id]:
+                print(f"Session {session_id} started")
             try:
                 data = queue.get(timeout=1)
                 if data is not None:
@@ -57,3 +65,10 @@ class GameServiceSingleton:
 
     def get_session_queues(self):
         return self.session_queues
+
+    def start_game(self, session_id):
+        if session_id in self.session_queues:
+            self.bGameStarted[session_id] = True
+            print(f"Starting game in session {session_id}")
+        else:
+            print(f"Session {session_id} not found")
